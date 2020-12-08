7b1f969b2c46cdb46eb0f434c7209a4d03ce074a
sdat-dev <sdat@albany.edu>
2020-12-08 17:05:03 -0500

    auto commit due to data change

--- js/conferenceformat.js
+++ js/conferenceformat.js
@@ -115,12 +115,18 @@ let addheader =  function (headers){
         {
             content += '<div class="carousel-item">';
         }
-        content +=  '<img src="'+ source + image +'" class="d-block w-100" alt="...">'+
-                    '<div id = "landing-page-text-wrapper">'+
-                        '<h1>'+ header1 +'</h1>' + 
-                        '<p>' + header2 + '</p>' +      
-                    '</div>'+
-                '</div>';
+        if(header1==''){
+            content +=  '<img src="'+ source + image +'" class="d-block w-100" alt="...">'+
+        '</div>';
+        }
+        else{
+            content +=  '<img src="'+ source + image +'" class="d-block w-100" alt="...">'+
+            '<div id = "landing-page-text-wrapper">'+
+                '<h1>'+ header1 +'</h1>' + 
+                '<p>' + header2 + '</p>' +      
+            '</div>'+
+        '</div>';
+        }
     }
     content +=  '</div></div>';
     header.innerHTML = content;
